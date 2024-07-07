2d448bd549d4ee4c4702b31259fa12407d8901c0
Daegyeom Ha <combbm@gmail.com>
2024-07-07 19:40:17 +0900

    feat: add default value of config

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,6 @@ import dotenvx from '@dotenvx/dotenvx';
 dotenvx.config();
 
 export const config = {
-  BOT_TOKEN: process.env.BOT_TOKEN,
-  ADMIN: process.env.ADMIN
+  BOT_TOKEN: process.env.BOT_TOKEN || '',
+  ADMIN: process.env.ADMIN || ''
 };
