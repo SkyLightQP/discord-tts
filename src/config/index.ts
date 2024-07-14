303255a0bcd1ce251ba1b82acdfe54d021465354
Daegyeom Ha <combbm@gmail.com>
2024-07-14 16:21:13 +0900

    feat: add config value

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,5 +4,6 @@ dotenvx.config();
 
 export const config = {
   BOT_TOKEN: process.env.BOT_TOKEN || '',
-  ADMIN: process.env.ADMIN || ''
+  ADMIN: process.env.ADMIN || '',
+  ALLOW_COMMAND_CHANNEL: process.env.ALLOW_COMMAND_CHANNEL || '',
 };
